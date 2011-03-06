03cc9b713e230e3cbe4adb6931e40d8c7b300f21
Jonathan Patt <jonathanpatt@gmail.com>
2011-03-05 23:33:03 -0700

    specified errorPage() as public function

--- ErrorCenter.class.php
+++ ErrorCenter.class.php
@@ -3,7 +3,7 @@ require_once 'NotificationCenter.class.php';
 
 class ErrorCenter extends NotificationCenter
 {
-    function errorPage($path, $type = null,
+    public function errorPage($path, $type = null,
         $prefix = '<li>', $suffix = "</li>\n"
     ) {
         if ($OftaErrors = $this->format($type, $prefix, $suffix)) {
