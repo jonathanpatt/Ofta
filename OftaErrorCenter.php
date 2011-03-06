1d0578a09243e64a04b62f876ed2376d0b3ee485
Jonathan Patt <jonathanpatt@gmail.com>
2011-03-05 22:24:23 -0700

    Changed spacing/formatting to conform to PEAR coding standard

--- OftaErrorCenter.php
+++ OftaErrorCenter.php
@@ -1,17 +1,15 @@
 <?php
-    /**
-    * OftaErrorCenter
-    * Extension of NotificationCenter, adding error-specific functionality
-    */
-    
-    require_once('OftaNotificationCenter.php');
+require_once 'OftaNotificationCenter.php';
 
-    class OftaErrorCenter extends OftaNotificationCenter {
-        function errorPage($path, $type = null, $prefix = '<li>', $suffix = "</li>\n") {
-            if ($OftaErrors = $this->format($type, $prefix, $suffix)) {
-                require $path;
-                die;
-            }
+class OftaErrorCenter extends OftaNotificationCenter
+{
+    function errorPage($path, $type = null,
+        $prefix = '<li>', $suffix = "</li>\n"
+    ) {
+        if ($OftaErrors = $this->format($type, $prefix, $suffix)) {
+            include $path;
+            die;
         }
     }
+}
 ?>
--- OftaNotificationCenter.php
+++ OftaNotificationCenter.php
@@ -1,39 +1,44 @@
 <?php
-    class OftaNotificationCenter {
-        private $messages = array();
-        
-        public function add($message, $type = null) {
-            if ($type) {
-                $this->messages[$type][] = $message;
-            } else {
-                $this->messages['general'][] = $message;
-            }
-        }
-        
-        public function messages($type = null) {
-            if ($type && $type != 'all') {
-                return $this->messages[$type];
-            } else {
-                $messages = array();
-                
-                foreach ($this->messages as $key => $value) {
-                    foreach ($value as $message) {
-                        $messages[] = $message;
-                    }
-                }
-                
-                return $messages;
-            }
-        }
-        
-        public function format($type = null, $prefix = '<li>', $suffix = "</li>\n") {
-            $formattedMessages = '';
-            
-            foreach ($this->messages($type) as $message) {
-                $formattedMessages .= $prefix.$message.$suffix;
-            }
-            
-            return $formattedMessages;
+class OftaNotificationCenter
+{
+    protected $messages = array();
+    
+    public function add($message, $type = null)
+    {
+        if ($type) {
+            $this->messages[$type][] = $message;
+        } else {
+            $this->messages['general'][] = $message;
         }
     }
+    
+    public function messages($type = null)
+    {
+        if ($type && $type != 'all') {
+            return $this->messages[$type];
+        } else {
+            $messages = array();
+            
+            foreach ($this->messages as $key => $value) {
+                foreach ($value as $message) {
+                    $messages[] = $message;
+                }
+            }
+            
+            return $messages;
+        }
+    }
+    
+    public function format($type = null, $prefix = '<li>',
+        $suffix = "</li>\n"
+    ) {
+        $formattedMessages = '';
+        
+        foreach ($this->messages($type) as $message) {
+            $formattedMessages .= $prefix.$message.$suffix;
+        }
+        
+        return $formattedMessages;
+    }
+}
 ?>
